932921a2822c470bf457c695bd1240f810e7244d
sravani-k-5 <sravanikondapallisravani@gmail.com>
2024-12-20 11:13:47 +0530

    react

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import React from 'react'
+import Text from './Text.jsx'
+
+function App() {
+  const str="sravs";
+  
+  return (
+    <>
+    <Text/>
+    <h1>{str}</h1>
+    <p>hello sravani this is react js !!!!!</p>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Text.jsx
@@ -0,0 +1,10 @@
+// import React from 'react'
+
+const Text = () => {
+  return (
+    <>
+    <div>sravani hello</div>
+    </>
+  )
+}
+export default Text;
